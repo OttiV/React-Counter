7e27c330927a10f15897bb0308b14a70a5683a1c
Vgnttv <o.vignani@gmail.com>
2019-08-09 13:20:22 +0200

    Implemented styling

--- src/components/Total.js
+++ src/components/Total.js
@@ -3,13 +3,13 @@ export default class Total extends Component {
   render() {
     const { counterTot, altData, extraData } = this.props;
     const tot = counterTot + extraData + parseFloat(altData)
-    return <div className="Total"><h1 class="title">Total = {tot}
+    return <div className="Total"><h1 class="title">Total = {tot}</h1>
     <a className="button is-info is-outlined" onClick={this.props.onDelete}>
     <span>Reset</span>
     <span class="icon is-small">
       <i class="fas fa-times"></i>
     </span>
-  </a></h1>
+  </a>
   </div>
   }
 }
